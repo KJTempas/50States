a31f91f9a4f42a283d935e797389edefe9403a0b
KJTempas <fw6927mm@go.minneapolis.edu>
2020-04-09 20:37:49 -0500

    prep for Heroku sl92

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ var express = require('express')
 var bodyParser = require('body-parser')
 
 var states_api = require('./routes/states.js')
+var path = require('path')
+
 //creates the express app
 var app = express()
+
+//serves the Vue app
+app.use(express.static(path.join(__dirname, 'client', 'client', 'dist')))
 
 app.use(bodyParser.json())
 //the app is required to used the states_api route(variable created above)
@@ -19,7 +24,7 @@ app.use(function (err, req, res, next) {
     res.status(500).send('Server error')
 })
 
-//start the server running
+//start the server running`
 var server = app.listen(process.env.PORT || 3000, function() {
     console.log('app running on port', server.address().port)
 })
